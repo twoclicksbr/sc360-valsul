05702015483ac46768708cbccb8975e25f7501d5
Alex Bethel <github@twoclicks.com.br>
2026-02-24 17:44:48 -0300

    feat: restrict API routes to api subdomain only

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,18 @@
+<!-- routes\api.php -->
+
 <?php
 
 use App\Http\Controllers\ModuleController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('valsul/{module}')->group(function () {
-    Route::get('/',          [ModuleController::class, 'index']);
-    Route::post('/',         [ModuleController::class, 'store']);
-    Route::get('{id}',       [ModuleController::class, 'show']);
-    Route::put('{id}',       [ModuleController::class, 'update']);
-    Route::patch('{id}',     [ModuleController::class, 'update']);
-    Route::delete('{id}',    [ModuleController::class, 'destroy']);
-    Route::patch('{id}/restore', [ModuleController::class, 'restore']);
+Route::group(['domain' => env('API_DOMAIN')], function () {
+    Route::prefix('valsul/{module}')->group(function () {
+        Route::get('/',              [ModuleController::class, 'index']);
+        Route::post('/',             [ModuleController::class, 'store']);
+        Route::get('{id}',           [ModuleController::class, 'show']);
+        Route::put('{id}',           [ModuleController::class, 'update']);
+        Route::patch('{id}',         [ModuleController::class, 'update']);
+        Route::delete('{id}',        [ModuleController::class, 'destroy']);
+        Route::patch('{id}/restore', [ModuleController::class, 'restore']);
+    });
 });
